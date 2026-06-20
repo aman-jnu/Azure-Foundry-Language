408360df2415ffa928bdd44a8abf456fb95fa86e
aman18 <amanjnu18@gmail.com>
2026-06-20 14:40:02 +0530

    Initial Commit

--- Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
+++ Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from playsound3 import playsound
 from dotenv import load_dotenv
 
-# Import namespaces
+
+# import namespaces
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 
 
@@ -19,12 +22,29 @@ def main():
         speech_file_path = Path(__file__).parent / "speech.mp3"
 
 
+      
         # Create the Azure OpenAI client
-        
+        token_provider = get_bearer_token_provider(                    
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
+
+        client = AzureOpenAI(
+            azure_endpoint=endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version="2025-03-01-preview"
+        )
+                
 
 
         # Generate speech and save to file
-        
+        with client.audio.speech.with_streaming_response.create(
+                    model=model_deployment,
+                    voice="alloy",
+                    input="My voice is my passport!",
+                    instructions="Speak in a serious tone.",
+                ) as response:
+            response.stream_to_file(speech_file_path)
+                
 
 
         # Play the generated speech file
